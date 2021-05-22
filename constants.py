af737e99fc23026b3ebba4e0cb17b00694c3a678
EVA-SAYAGH <84644112+EVA-SAYAGH@users.noreply.github.com>
2021-05-22 23:12:05 +0200

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,59 @@
+words = {
+    "normal": {
+        "noun": {
+            "espece de": 1,
+            "maraud": 1,
+            "gredin": 2,
+            "cornichon": 1,
+            "malotru": 1,
+            "goujat": 1,
+        },
+        "liaison": {
+            "et": 0,
+            "en plus": 0,
+            "mais ce n'est pas tout": 1,
+            "cependant": 0,
+            "alors que": 0,
+            "comme": 0,
+            "de la même manière que": 0,
+            "mieux que": 0,
+            "moins bien que": 0,
+        },
+        "verb": {
+            "va": 1,
+            "fais": 1,
+            "dors": 1
+        }
+    },
+    "weight": {
+        "noun": {
+            "patate": 2,
+            "ventre": 2,
+            "bidon": 2,
+        },
+        "verb": {
+            "mange": 2,
+            "pese": 3,
+            "ecrase": 1
+        }
+    },
+    "coding": {
+        "noun": {
+            "ton PC": 2,
+            "ton algo": 2,
+            "developeur HTML": 10,
+            "complexité exponentielle": 2,
+            "boucle infiniement": 4,
+        },
+        "verbe": {
+            "rame": 2,
+            "dev": 1,
+        }
+    },
+    "familly": {
+        "noun": {
+            "ton père": 2,
+            "ta mère": 2,
+        }
+    }
+}
